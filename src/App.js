344e5ae6f11dc94573c0bc7cfdbece7e52f04c5c
Hath-W <hathwong@gmail.com>
2024-04-01 23:10:26 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './KGF.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Connecting you with everyone.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/Hath-W/CSCI3100-Kingfisher"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Kingfisher
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
