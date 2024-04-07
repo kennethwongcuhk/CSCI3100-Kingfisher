bea95bef2b85bdfa0e60cde3754b83a57fca12da
kennotfindsymbol <kennotfindsymbol@gmail.com>
2024-04-07 23:38:21 +0800

    Add jwt in signupUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn:'1d' });
+}
 
 const loginUser = async (req, res) => {
     res.json({mssg: 'login user'});
@@ -8,7 +13,8 @@ const signupUser = async (req, res) => {
     const {email, username, password} = req.body;
     try {
         const user = await User.signup(email, username, password);
-        res.status(200).json({username, user})
+        const token = createToken(user._id);
+        res.status(200).json({username, token})
     } catch (error) {
         res.status(400).json({error: error.message});
     }
