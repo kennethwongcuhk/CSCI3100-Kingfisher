7d12bee9226f89edd9e02ea87c207d3e241c8dbd
kennotfindsymbol <kennotfindsymbol@gmail.com>
2024-04-07 17:48:09 +0800

    Add basic user schema

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const tweetRoutes = require('./routes/tweets');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -13,7 +14,8 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/api/tweets', tweetRoutes)
+app.use('/api/tweets', tweetRoutes);
+app.use('/api/user', userRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
