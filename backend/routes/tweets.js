f9efe95ad1a887d1cb2e0ac4e483ed25c30473fb
kennotfindsymbol <kennotfindsymbol@gmail.com>
2024-04-08 09:19:54 +0800

    Add requireAuth middleware

--- backend/routes/tweets.js
+++ backend/routes/tweets.js
@@ -5,8 +5,11 @@ const {
     createTweet,
     deleteTweet,
 } = require('../controllers/tweetController')
+const requireAuth = require('../middleware/requireAuth');
 
 const router = express.Router();
+
+router.use(requireAuth);
 
 router.get('/', getTweets);
 
